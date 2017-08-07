e4622739cac0bea42e3bfda58a9ae3394bdceb4e
ValentinHO <valentinhernandezortega@gmail.com>
2017-08-06 22:01:45 -0500

    Primer commit 06-08-17

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,55 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class PersonaController : Controller
+    {
+        //
+        // GET: /Persona/
+        public ActionResult Persona()
+        {
+            Persona obj = new Persona();
+            obj.edad = Convert.ToInt32(Request.Form["edad"]);
+            obj.nombre = Request.Form["nombre"].ToString();
+            obj.apellido = Request.Form["apellido"].ToString();
+            return View(obj);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Buscar()
+        {
+            String nombreProducto = RouteData.Values["nombreProducto"].ToString();
+            nombreProducto = Server.HtmlEncode(nombreProducto);
+            String resultado = String.Empty;
+
+            switch (nombreProducto)
+            {
+                case "PC":
+                    {
+                        resultado = "Disponibles 4 unidades";
+                        break;
+                    }
+                case "RAM":
+                    {
+                        resultado = "Disponibles 2 unidades";
+                        break;
+                    }
+                default:
+                    {
+                        resultado = String.Format("El producto {0} no existe en stock",nombreProducto);
+                        break;
+                    }
+            }
+            return Content("<p>"+ resultado +"</p>");
+        }
+	}
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Persona
+    {
+        public int edad { get; set; }
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+    }
+}
